15ac25238680ffe6cdeb97609a0df0680a72b4e1
FirstSportz <nikhil.chindarkar@ibm.com>
2024-10-16 19:45:36 +0530

    adding brevo email configuration.

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,4 +1,4 @@
-export default ({ strapi }) => ({
+export default ({ env }) => ({
     // Extend the users-permissions plugin configuration
     'users-permissions': {
       config: {
@@ -8,27 +8,18 @@ export default ({ strapi }) => ({
       },
     },
   
-    // Extending the findOne method to populate categories
-    bootstrap() {
-      const extensionService = strapi.plugin('users-permissions').service('user');
-  
-      const originalFindOne = extensionService.findOne;
-  
-      extensionService.findOne = async (id, params) => {
-        // Initialize params if it's undefined
-        params = params || {};
-  
-        // Ensure categories are populated when fetching user data
-        if (!params.populate) {
-          params.populate = [];
-        }
-  
-        // Add categories to the populate array
-        params.populate.push('categories');
-  
-        // Call the original findOne method with the modified params
-        return originalFindOne.call(extensionService, id, params);
-      };
-    },
+    email: {
+        config: {
+          provider: 'strapi-provider-email-brevo',
+          providerOptions: {
+            apiKey: env('BREVO_API_KEY'),
+          },
+          settings: {
+            defaultFrom: 'aditya.mahajan@firstsportz.com',
+            defaultSenderName: 'FirstSportz',
+            defaultReplyTo: 'aditya.mahajan@firstsportz.com',
+          },
+        },
+      },
   });
   
