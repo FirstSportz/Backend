76676968578f15580e63d002922a96eb93f41260
FirstSportz <nikhil.chindarkar@ibm.com>
2024-10-16 19:37:19 +0530

    Revert "adding brevo email configuration."
    This reverts commit bd39ee20a02a1b57d0f2d087271b09b4df331cfc.

--- config/plugins.ts
+++ config/plugins.ts
@@ -12,7 +12,7 @@ export default ({ env }) => ({
         config: {
           provider: 'strapi-provider-email-brevo',
           providerOptions: {
-            apiKey: env('BREVO_API_KEY'),
+            apiKey: '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',
           },
           settings: {
             defaultFrom: 'aditya.mahajan@firstsportz.com',
